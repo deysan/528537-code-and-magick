d3f22dd9a6416042f06ca33bd755858c11d772a6
deysan <ideysan@gmail.com>
2018-12-05 00:20:05 +0200

    Подготовил параметры для персонажей

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,28 @@
 'use strict';
 
 document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var firstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var random = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = random(firstName) + ' ' + random(lastName);
+  wizardElement.querySelector('.wizard-coat').style.fill = random(coatColor);
+  wizardElement.querySelector('.wizard-eyes').style.fill = random(eyesColor);
+
+  similarListElement.appendChild(wizardElement);
+}
