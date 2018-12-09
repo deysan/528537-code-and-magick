c8c7708ec9e03cb38c206dd597062fec226e578c
deysan <ideysan@gmail.com>
2018-12-09 11:05:26 +0200

    Добавил валидацию форм

--- js/setup.js
+++ js/setup.js
@@ -65,16 +65,40 @@ var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
 
-setupOpen.addEventListener('click', function() {
+setupOpen.addEventListener('click', function () {
   setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+      setup.classList.add('hidden');
+    }
+  });
 });
 
-setupClose.addEventListener('click', function() {
+setupClose.addEventListener('click', function () {
   setup.classList.add('hidden');
 });
 
-document.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    setup.classList.add('hidden');
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
   }
 });
