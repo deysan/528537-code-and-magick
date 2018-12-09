0b5fb70266ca93210fe3594b48bc42dff4b581bc
deysan <ideysan@gmail.com>
2018-12-09 11:17:03 +0200

    Обновил код показа окна с настройками

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,8 @@ var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вал
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_NUMBER = 4;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var userDialog = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -64,29 +66,49 @@ var init = function () {
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
-
-setupOpen.addEventListener('click', function () {
-  setup.classList.remove('hidden');
-
-  document.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27) {
-      setup.classList.add('hidden');
-    }
-  });
-});
-
-setupClose.addEventListener('click', function () {
-  setup.classList.add('hidden');
-});
-
-
 var userNameInput = setup.querySelector('.setup-user-name');
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
 
 userNameInput.addEventListener('invalid', function (evt) {
   if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    userNameInput.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
   } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    userNameInput.setCustomValidity('Максимальная длина имени персонажа — 25 символов');
   } else if (userNameInput.validity.valueMissing) {
     userNameInput.setCustomValidity('Обязательное поле');
   } else {
@@ -97,7 +119,7 @@ userNameInput.addEventListener('invalid', function (evt) {
 userNameInput.addEventListener('input', function (evt) {
   var target = evt.target;
   if (target.value.length < 2) {
-    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    target.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
   } else {
     target.setCustomValidity('');
   }
