d3043848a9177014b15fe24b465fd556f5bf31e8
deysan <ideysan@gmail.com>
2018-12-14 00:11:58 +0200

    Переименовал переменные

--- js/setup.js
+++ js/setup.js
@@ -9,21 +9,21 @@ var WIZARDS_NUMBER = 4;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
-var userDialog = document.querySelector('.setup');
+var userDialogElement = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var userDialogOpen = document.querySelector('.setup-open');
-var userDialogClose = userDialog.querySelector('.setup-close');
-var userNameInput = userDialog.querySelector('.setup-user-name');
-var coatWizard = document.querySelector('.setup-wizard .wizard-coat');
-var inputCoatWizard = document.querySelector('.setup-wizard-form').querySelector('[name="coat-color"]');
-var eyesWizard = document.querySelector('.setup-wizard .wizard-eyes');
-var inputEyesWizard = document.querySelector('.setup-wizard-form').querySelector('[name="eyes-color"]');
-var fireballWizard = document.querySelector('.setup-fireball-wrap');
-var inputFireballWizard = fireballWizard.querySelector('input');
+var userDialogOpenElement = document.querySelector('.setup-open');
+var userDialogCloseElement = userDialogElement.querySelector('.setup-close');
+var userNameInputElement = userDialogElement.querySelector('.setup-user-name');
+var coatWizardElement = document.querySelector('.setup-wizard .wizard-coat');
+var inputCoatWizardElement = document.querySelector('.setup-wizard-form').querySelector('[name="coat-color"]');
+var eyesWizardElement = document.querySelector('.setup-wizard .wizard-eyes');
+var inputEyesWizardElement = document.querySelector('.setup-wizard-form').querySelector('[name="eyes-color"]');
+var fireballWizardElement = document.querySelector('.setup-fireball-wrap');
+var inputFireballWizardElement = fireballWizardElement.querySelector('input');
 
 var getRandomElementFromArray = function (array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -70,57 +70,55 @@ init();
 
 // Настройки персонажа
 var onPopupEscPress = function (evt) {
-  if (evt.target === userNameInput) {
-    return;
-  } else if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.target !== userNameInputElement && evt.keyCode === ESC_KEYCODE) {
     closePopup();
   }
 };
 
 var openPopup = function () {
-  userDialog.classList.remove('hidden');
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  userDialogElement.classList.remove('hidden');
+  userDialogElement.querySelector('.setup-similar').classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
 var closePopup = function () {
-  userDialog.classList.add('hidden');
+  userDialogElement.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
-userDialogOpen.addEventListener('click', function () {
+userDialogOpenElement.addEventListener('click', function () {
   openPopup();
 });
 
-userDialogOpen.addEventListener('keydown', function (evt) {
+userDialogOpenElement.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     openPopup();
   }
 });
 
-userDialogClose.addEventListener('click', function () {
+userDialogCloseElement.addEventListener('click', function () {
   closePopup();
 });
 
-userDialogClose.addEventListener('keydown', function (evt) {
+userDialogCloseElement.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
 });
 
-userNameInput.addEventListener('invalid', function () {
-  if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
-  } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('Максимальная длина имени персонажа — 25 символов');
-  } else if (userNameInput.validity.valueMissing) {
-    userNameInput.setCustomValidity('Обязательное поле');
+userNameInputElement.addEventListener('invalid', function () {
+  if (userNameInputElement.validity.tooShort) {
+    userNameInputElement.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
+  } else if (userNameInputElement.validity.tooLong) {
+    userNameInputElement.setCustomValidity('Максимальная длина имени персонажа — 25 символов');
+  } else if (userNameInputElement.validity.valueMissing) {
+    userNameInputElement.setCustomValidity('Обязательное поле');
   } else {
-    userNameInput.setCustomValidity('');
+    userNameInputElement.setCustomValidity('');
   }
 });
 
-userNameInput.addEventListener('input', function (evt) {
+userNameInputElement.addEventListener('input', function (evt) {
   var target = evt.target;
   if (target.value.length < 2) {
     target.setCustomValidity('Имя персонажа не может содержать менее 2 символов');
@@ -130,20 +128,20 @@ userNameInput.addEventListener('input', function (evt) {
 });
 
 // Изменение цвета персонажа
-coatWizard.addEventListener('click', function () {
+coatWizardElement.addEventListener('click', function () {
   var color = getRandomElementFromArray(COAT_COLORS);
-  coatWizard.style.fill = color;
-  inputCoatWizard.setAttribute('value', color);
+  coatWizardElement.style.fill = color;
+  inputCoatWizardElement.setAttribute('value', color);
 });
 
-eyesWizard.addEventListener('click', function () {
+eyesWizardElement.addEventListener('click', function () {
   var color = getRandomElementFromArray(EYES_COLORS);
-  eyesWizard.style.fill = color;
-  inputEyesWizard.setAttribute('value', color);
+  eyesWizardElement.style.fill = color;
+  inputEyesWizardElement.setAttribute('value', color);
 });
 
-fireballWizard.addEventListener('click', function () {
+fireballWizardElement.addEventListener('click', function () {
   var color = getRandomElementFromArray(FIREBALL_COLORS);
-  fireballWizard.style.background = color;
-  inputFireballWizard.setAttribute('value', color);
+  fireballWizardElement.style.background = color;
+  inputFireballWizardElement.setAttribute('value', color);
 });
