1dbe110b81dd532dbfb2f68f5ac898947b981fa4
deysan <ideysan@gmail.com>
2018-12-26 00:49:51 +0200

    Добавил драгндроп окна редактирования персонажа

--- js/dialog.js
+++ js/dialog.js
@@ -1,1 +1,52 @@
 'use strict';
+
+var uploadElement = userDialogElement.querySelector('.upload');
+
+uploadElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userDialogElement.style.top = (userDialogElement.offsetTop - shift.y) + 'px';
+    userDialogElement.style.left = (userDialogElement.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        uploadElement.removeEventListener('click', onClickPreventDefault)
+      };
+      uploadElement.addEventListener('click', onClickPreventDefault);
+    }
+
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
